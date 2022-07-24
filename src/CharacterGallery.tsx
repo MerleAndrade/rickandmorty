27d717a6c29e66109725176228ead6f89419b490
merleandrade <andrade.merle@gmail.com>
2022-07-24 10:29:55 +0200

    Add Rick&Morty Gallery

---
+++ src/CharacterGallery.tsx
@@ -0,0 +1,19 @@
+//alle Charaktere nehmen und anzeigen
+
+import {Character} from "./Character";
+import CharacterCard from "./CharacterCard";
+
+type CharacterGalleryProps = {
+        chars: Character[]
+}
+
+export default function CharacterGallery(props: CharacterGalleryProps) {
+
+    return(
+    <div>
+        {props.chars.map((char) => <CharacterCard char={char}/>)
+                }
+    </div>
+
+    )
+}
